b4dbdd1a6df9725987bb793d404266d60c4b4b48
Yacine-08 <mbayey@ept.sn>
2026-05-19 13:08:03 +0000

    Initial commit

---
+++ src/test/java/sn/edu/ept/mediconnect/MediconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.edu.ept.mediconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
